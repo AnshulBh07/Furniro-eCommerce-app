8c6a2be45cabb9e186d581037c63ddd16709fc98
Anshul Bhardwaj <anshulbh009@gmail.com>
2023-11-04 10:45:02 +0530

    error handling at server

--- server/server.js
+++ server/server.js
@@ -4,18 +4,17 @@ import cors from "cors";
 import md5 from "md5";
 import { sendMail } from "./services/sendMail.js";
 import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 const prisma = new PrismaClient();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-const options = {
-  origin: "http://localhost:3000",
-};
-
-app.use(cors(options));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("On the server");
@@ -24,72 +23,104 @@ app.get("/", (req, res) => {
 app.get("/products", async (req, res) => {
   // findMany returns all the rows specified in arguments of function
   //as no argument is specified we get all rows
-  const allProducts = await prisma.products.findMany({
-    include: {
-      product_details: {
-        select: { brand_name: true, room_type: true },
+  try {
+    const allProducts = await prisma.products.findMany({
+      include: {
+        product_details: {
+          select: { brand_name: true, room_type: true },
+        },
       },
-    },
-  });
-  res.send(allProducts);
+    });
+
+    res.send(allProducts);
+  } catch (err) {
+    if (err) console.error(err);
+  }
 });
 
 app.get("/product_details/:sku", async (req, res) => {
   const value = String(req.params.sku);
   // in prisma we can call nested queries as tables are referenced ,we can also set which column to get
   // using select-true method
-  const product_info = await prisma.products.findUnique({
-    where: { sku: value },
-    include: {
-      product_details: true,
-    },
-  });
-  res.send(product_info);
+  try {
+    const product_info = await prisma.products.findUnique({
+      where: { sku: value },
+      include: {
+        product_details: true,
+      },
+    });
+
+    res.send(product_info);
+  } catch (err) {
+    if (err) console.error(err);
+  }
 });
 
 app.get("/related_products", async (req, res) => {
   const category = String(req.query.category);
 
-  const related_items = await prisma.products.findMany({
-    where: { category: category },
-  });
+  try {
+    const related_items = await prisma.products.findMany({
+      where: { category: category },
+    });
 
-  res.send(related_items);
+    res.send(related_items);
+  } catch (err) {
+    if (err) console.error(err);
+  }
 });
 
 app.get("/categories", async (req, res) => {
-  const allCategories = await prisma.products.findMany({
-    distinct: ["category"],
-    select: { category: true },
-  });
-  res.send(allCategories);
+  try {
+    const allCategories = await prisma.products.findMany({
+      distinct: ["category"],
+      select: { category: true },
+    });
+
+    res.send(allCategories);
+  } catch (err) {
+    if (err) console.error(err);
+  }
 });
 
 app.get("/room_types", async (req, res) => {
-  const allRooms = await prisma.product_details.findMany({
-    distinct: ["room_type"],
-    select: { room_type: true },
-  });
+  try {
+    const allRooms = await prisma.product_details.findMany({
+      distinct: ["room_type"],
+      select: { room_type: true },
+    });
 
-  res.send(allRooms);
+    res.send(allRooms);
+  } catch (err) {
+    if (err) console.error(err);
+  }
 });
 
 app.get("/brands", async (req, res) => {
-  const allBrands = await prisma.product_details.findMany({
-    distinct: ["brand_name"],
-    select: { brand_name: true },
-  });
-  res.send(allBrands);
+  try {
+    const allBrands = await prisma.product_details.findMany({
+      distinct: ["brand_name"],
+      select: { brand_name: true },
+    });
+
+    res.send(allBrands);
+  } catch (err) {
+    if (err) console.error(err);
+  }
 });
 
 app.get("/getUser", async (req, res) => {
   const email = String(req.query.mail);
 
-  const user_info = await prisma.users.findMany({
-    where: { email: email },
-  });
+  try {
+    const user_info = await prisma.users.findMany({
+      where: { email: email },
+    });
 
-  res.send(user_info);
+    res.send(user_info);
+  } catch (err) {
+    if (err) console.error(err);
+  }
 });
 
 app.post("/new_user", async (req, res) => {
@@ -99,17 +130,22 @@ app.post("/new_user", async (req, res) => {
   const email = req.body.email;
   const pass = md5(req.body.pwd1);
 
-  const userInserted = await prisma.users.create({
-    data: {
-      first_name: fname,
-      last_name: lname,
-      email: email,
-      password_hash: pass,
-      hasher: "md5",
-    },
-  });
+  try {
+    const userInserted = await prisma.users.create({
+      data: {
+        first_name: fname,
+        last_name: lname,
+        email: email,
+        password_hash: pass,
+        hasher: "md5",
+      },
+    });
+    console.log(userInserted);
 
-  res.send("Sign up successfull!.");
+    res.send("Sign up successfull!.");
+  } catch (err) {
+    if (err) console.error(err);
+  }
 });
 
 app.post("/user_login", (req, res) => {});
